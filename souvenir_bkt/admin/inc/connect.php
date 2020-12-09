d43a163bda04751e3503b3462787f3e9f054b474
MuhammadAgungMahardhika <58037995+MuhammadAgungMahardhika@users.noreply.github.com>
2020-12-09 14:35:58 +0700

    Update connect.php

--- souvenir_bkt/admin/inc/connect.php
+++ souvenir_bkt/admin/inc/connect.php
@@ -1,8 +1,8 @@
 <?php
-	$host = "localhost";
-	$user = "postgres";
-	$pass = "simpatiku76";
+	$host = "ec2-50-19-247-157.compute-1.amazonaws.com";
+	$user = "zjxoqslzlydifd";
+	$pass = "fe8e480b7436f8ab284ec79cd8d57e2d39d2d09731d419573b76b8acbd1ce589";
 	$port = "5432";
-	$dbname = "bkt_tourism";
+	$dbname = "d9tldco333afrj";
 	$conn = pg_connect("host=".$host." port=".$port." dbname=".$dbname." user=".$user." password=".$pass) or die("Gagal");
 ?>
